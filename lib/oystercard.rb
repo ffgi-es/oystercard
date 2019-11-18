6a80641c376f808b58174a880aec08fff1cb1c85
Student <student@Makerss-MacBook-Pro.local>
2019-11-18 16:21:49 +0000

    added touch_in, touch_out & in_journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,13 +4,31 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
+    raise top_up_amount_error(amount) unless amount > 0
     total = amount + @balance
     raise balance_limit_error(total) if total > BALANCE_LIMIT
 
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
 
   private
@@ -19,7 +37,15 @@ class Oystercard
     message = "Over balance limit (#{total}/#{BALANCE_LIMIT})"
     BalanceError.new(message)
   end
+
+  def top_up_amount_error amount
+    message = "Cannot top up with amount of #{amount}"
+    InvalidOperationError.new(message)
+  end
 end
 
 class BalanceError < StandardError
+end
+
+class InvalidOperationError < StandardError
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,6 +7,9 @@ describe Oystercard do
     it "should have a balance of 0" do
       expect(subject.balance).to eq 0
     end
+    it "should not be in a journey" do
+      expect(card).to_not be_in_journey
+    end
   end
 
   describe "#top_up" do
@@ -21,6 +24,11 @@ describe Oystercard do
       expect(card.balance).to eq 3
     end
 
+    specify "that it raises an error when the amount isn't greater than 0" do
+      expect{card.top_up -1}.to raise_error InvalidOperationError, "Cannot top up with amount of -1"
+      expect{card.top_up 0}.to raise_error InvalidOperationError, "Cannot top up with amount of 0"
+    end
+
     context "When balance limit is reached" do
       it "should raise an error if the amount is over the limit" do
         expect { card.top_up 91 }.to raise_error BalanceError, "Over balance limit (91/90)"
@@ -29,6 +37,26 @@ describe Oystercard do
       it "should raise an error if the balance would go over the limit" do
         card.top_up 50
         expect { card.top_up 50 }.to raise_error BalanceError, "Over balance limit (100/90)"
+      end
+    end
+  end
+  context 'an oystercard with money on it' do
+    before { card.top_up 40 }
+    describe '#deduct' do
+      it 'should decrease the balance' do
+        card.deduct 5 
+        expect(card.balance).to eq 35
+      end
+    end
+    context 'going on a journey' do
+      it 'should be possible to touch in' do
+        card.touch_in
+        expect(card.in_journey?).to eq true
+      end
+      it 'should be possible to touch_out' do
+        card.touch_in
+        card.touch_out
+        expect(card.in_journey?).to eq false
       end
     end
   end
