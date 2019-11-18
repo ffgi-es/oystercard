3ec320a0858ff382e87056fb347dbea47c1cc801
Student <student@Makerss-MacBook-Pro.local>
2019-11-18 14:58:36 +0000

    implemented top_up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,21 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:card) { described_class.new }
   context "Initialised card" do
     it "should have a balance of 0" do
       expect(subject.balance).to eq 0
+    end
+  end
+  describe "#top_up" do
+    specify "that top up increases the balance" do
+      card.top_up 1
+      expect(card.balance).to eq 1
+    end
+    specify "that multiple top ups are recorded" do
+      card.top_up 1
+      card.top_up 2
+      expect(card.balance).to eq 3
     end
   end
 end
