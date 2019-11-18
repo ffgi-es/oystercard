edd7986f01947c11eefc1944c05af3ad8b26eb9a
Alastair Gilles <agilles91@gmail.com>
2019-11-18 16:35:11 +0100

    Added a balance limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,5 @@
 class Oystercard
+  BALANCE_LIMIT = 90
   attr_reader :balance
 
   def initialize
@@ -6,6 +7,19 @@ class Oystercard
   end
 
   def top_up(amount)
+    total = amount + @balance
+    raise balance_limit_error(total) if total > BALANCE_LIMIT
+
     @balance += amount
   end
+
+  private
+
+  def balance_limit_error total
+    message = "Over balance limit (#{total}/#{BALANCE_LIMIT})"
+    BalanceError.new(message)
+  end
+end
+
+class BalanceError < StandardError
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,20 +2,34 @@ require 'oystercard'
 
 describe Oystercard do
   subject(:card) { described_class.new }
+
   context "Initialised card" do
     it "should have a balance of 0" do
       expect(subject.balance).to eq 0
     end
   end
+
   describe "#top_up" do
     specify "that top up increases the balance" do
       card.top_up 1
       expect(card.balance).to eq 1
     end
+
     specify "that multiple top ups are recorded" do
       card.top_up 1
       card.top_up 2
       expect(card.balance).to eq 3
+    end
+
+    context "When balance limit is reached" do
+      it "should raise an error if the amount is over the limit" do
+        expect { card.top_up 91 }.to raise_error BalanceError, "Over balance limit (91/90)"
+      end
+
+      it "should raise an error if the balance would go over the limit" do
+        card.top_up 50
+        expect { card.top_up 50 }.to raise_error BalanceError, "Over balance limit (100/90)"
+      end
     end
   end
 end
